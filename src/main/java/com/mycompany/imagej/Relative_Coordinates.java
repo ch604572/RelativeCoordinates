67b89bcf997958198284b00ecab229571bd59a06
ch604572 <44028123+ch604572@users.noreply.github.com>
2018-11-19 15:25:39 -0400

    Update Relative_Coordinates.java

--- src/main/java/com/mycompany/imagej/Relative_Coordinates.java
+++ src/main/java/com/mycompany/imagej/Relative_Coordinates.java
@@ -7,23 +7,24 @@ import ij.plugin.PlugIn;
 
 
 public class Relative_Coordinates implements PlugIn {
-	public double x_translated;
-	public double y_translated;
+	ImagePlus imp = IJ.getImage();	
+	public double x_translated = imp.getCalibration().xOrigin;
+	public double y_translated = imp.getCalibration().yOrigin;
 	public double angleDeg;
+	
 	@Override
 	public void run(String arg) {
 		if( showDialog() ) return;
 		RelativeCoordinates coordinates = new RelativeCoordinates(x_translated, y_translated, angleDeg);
 	}
-	
-	
+		
 	private boolean showDialog() {
 		GenericDialog gd = new GenericDialog("Relative Coordinates");
 		
-		ImagePlus imp = IJ.getImage();		
+			
 		// default value is 0.00, 2 digits right of the decimal point
-		gd.addNumericField( "x-coordinate", imp.getCalibration().xOrigin, 3 );
-		gd.addNumericField( "y-coordinate", imp.getCalibration().yOrigin, 3 );
+		gd.addNumericField( "x-translated", x_translated, 3 );
+		gd.addNumericField( "y-translated", y_translated, 3 );
 		gd.addNumericField( "Angle (deg) ", 0.00, 2);
 
 		gd.showDialog();
@@ -39,5 +40,4 @@ public class Relative_Coordinates implements PlugIn {
 	}
 	
 }
-
 
