25b4af0553911da5dd39a0b11434715843370d97
ch604572 <44028123+ch604572@users.noreply.github.com>
2018-11-19 15:13:34 -0400

    Added generic dialog

--- src/main/java/com/mycompany/imagej/Relative_Coordinates.java
+++ src/main/java/com/mycompany/imagej/Relative_Coordinates.java
@@ -2,19 +2,42 @@ package com.mycompany.imagej;
 
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import ij.plugin.PlugIn;
 
 
 public class Relative_Coordinates implements PlugIn {
-		// Use to massage the data retrieved from trackmate
-	// since trackmate does not use the origin set in ImageJ 
-	// nor the updated scales and origins after its model is set
-	
+	public double x_translated;
+	public double y_translated;
+	public double angleDeg;
 	@Override
 	public void run(String arg) {
-		RelativeCoordinates coordinates = new RelativeCoordinates();
-		RelativeCoordinatesFrame frame = new  RelativeCoordinatesFrame();
+		if( showDialog() ) return;
+		RelativeCoordinates coordinates = new RelativeCoordinates(x_translated, y_translated, angleDeg);
+	}
+	
+	
+	private boolean showDialog() {
+		GenericDialog gd = new GenericDialog("Relative Coordinates");
+		
+		ImagePlus imp = IJ.getImage();		
+		// default value is 0.00, 2 digits right of the decimal point
+		gd.addNumericField( "x-coordinate", imp.getCalibration().xOrigin, 3 );
+		gd.addNumericField( "y-coordinate", imp.getCalibration().yOrigin, 3 );
+		gd.addNumericField( "Angle (deg) ", 0.00, 2);
+
+		gd.showDialog();
+		if (gd.wasCanceled()) {
+			return true;
+		}
+		// get entered values
+		x_translated = gd.getNextNumber();
+		y_translated = gd.getNextNumber();
+		angleDeg = gd.getNextNumber();
+		
+		return false;
 	}
 	
 }
+
 
