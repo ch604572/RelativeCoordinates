6fa210458d7b7621d7061c0202c12f6944e2a102
ch604572 <44028123+ch604572@users.noreply.github.com>
2018-11-26 12:52:22 -0400

    Revised methods.

--- src/main/java/com/mycompany/imagej/RelativeCoordinates.java
+++ src/main/java/com/mycompany/imagej/RelativeCoordinates.java
@@ -15,98 +15,64 @@ public class RelativeCoordinates {
 	
 	private double angleDeg;
 	
-	protected double x_rotated;
-	
-	protected double y_rotated;
-	
+	/*
+	 *  Constructors 
+	 */
 	public RelativeCoordinates( ) {
 		ImagePlus imp = IJ.getImage();
 		this.xOrigin = imp.getCalibration().xOrigin;
 		this.yOrigin = imp.getCalibration().yOrigin;
-		IJ.log("new StrainActionRelativeCoordinates created. " + this.xOrigin + "  " + this.yOrigin);
-	}
-	
-	public RelativeCoordinates( final double x, final double y ) {
-		ImagePlus imp = IJ.getImage();
-		this.xOrigin = imp.getCalibration().xOrigin;
-		this.yOrigin = imp.getCalibration().yOrigin;
-		this.x_translated = x - this.xOrigin;
-		this.y_translated = y - this.yOrigin;	
-		IJ.log("new StrainActionRelativeCoordinates created. x_translated = " + this.x_translated + " y_translated = " + this.y_translated) ;
 	}
 	
 	public RelativeCoordinates( final double angleDeg ) {
-		ImagePlus imp = IJ.getImage();
-		this.xOrigin = imp.getCalibration().xOrigin;
-		this.yOrigin = imp.getCalibration().yOrigin;
-		this.x_translated = xOrigin;
-		this.y_translated = yOrigin;
+		this.angleDeg = angleDeg;		
+	}
+	
+	public RelativeCoordinates( final double xOrigin, final double yOrigin ) {
+		this.xOrigin = xOrigin;
+		this.yOrigin = yOrigin;		
+	}	
+
+	public RelativeCoordinates( final double xOrigin, final double yOrigin, final double angleDeg ) {
+		this.xOrigin = xOrigin;
+		this.yOrigin = yOrigin;	
 		this.angleDeg = angleDeg;
-		setXRotated(x_translated, y_translated);
-		setYRotated(x_translated, y_translated);		
-		IJ.log("new StrainActionRelativeCoordinates created. angleDeg = " + this.angleDeg + " x_rotated = " + this.x_rotated + " y_rotated = " + this.y_rotated);
 	}
 	
-	public RelativeCoordinates( final double x, final double y, final double angleDeg ) {
-		ImagePlus imp = IJ.getImage();
-		this.xOrigin = imp.getCalibration().xOrigin;
-		this.yOrigin = imp.getCalibration().yOrigin;
-		this.x_translated = x - xOrigin;
-		this.y_translated = y - yOrigin;
-		this.angleDeg = angleDeg;
-		setXRotated(x_translated, y_translated);
-		setYRotated(x_translated, y_translated);		
-		IJ.log("new StrainActionRelativeCoordinates created. x_translated = " + this.x_translated + " y_translated = " + this.y_translated + " angleDeg = " + this.angleDeg
-				+ " x_rotated = " + this.x_rotated + " y_rotated = " + this.y_rotated);
-	}
-	
-	public void setXTranslated( final double x )
-	{
-		this.x_translated = x - this.xOrigin;
-	}
-	
-	public void setYTranslated( final double y )
-	{
-		this.y_translated = y - this.yOrigin;
-	}
-	
+	/*
+	 *  Methods
+	 */
 	public void setAngleDeg( final double angleDeg )
 	{
 		this.angleDeg = angleDeg;
-	}
-		
-	public void setXRotated( final double x_translated, final double y_translated )
-	{
-		this.x_rotated = x_translated * Math.cos(Math.toRadians(angleDeg)) + y_translated * Math.sin(Math.toRadians(angleDeg));
-	}
-	
-	public void setYRotated( final double x_translated, final double y_translated )
-	{
-		this.y_rotated = - x_translated * Math.sin(Math.toRadians(angleDeg)) + y_translated * Math.cos(Math.toRadians(angleDeg));
-	}
+	}	
 	
 	public double getAngleDeg()
 	{
 		return angleDeg;
 	}
-	
-	public double getXTranslated()
+
+	public double xTranslated( final double x )
 	{
-		return x_translated;
+		return ( x - this.xOrigin );
 	}
 	
-	public double getYTranslated()
+	public double yTranslated( final double y )
 	{
-		return y_translated;
+		return ( y - this.yOrigin );
 	}
 	
-	public double getXRotated()
+	public double XRotated( final double x, final double y )
 	{
-		return y_rotated;
+		final double x_translated = xTranslated( x );
+		final double y_translated = yTranslated( y );
+		return x_translated * Math.cos( Math.toRadians( angleDeg ) ) + y_translated * Math.sin( Math.toRadians( angleDeg ) );
 	}
 	
-	public double getYRotated()
+	public double YRotated( final double x, final double y )
 	{
-		return y_rotated;
+		final double x_translated = xTranslated( x );
+		final double y_translated = yTranslated( y );
+		return -x_translated * Math.sin( Math.toRadians( angleDeg ) ) + y_translated * Math.cos( Math.toRadians( angleDeg ) );
 	}
 }
